9b9bd010c47c7ce98fff5c5a33dea401f3d67fae
Dhiman <dhimanjyoti62@gmail.com>
2023-06-26 13:31:42 +0530

    first program is added

---
+++ dist/1_hello_world.js
@@ -0,0 +1,25 @@
+"use strict";
+let dhiman = "Dhiman";
+console.log(dhiman);
+const chris = 45;
+console.log(typeof chris);
+const student = {
+    name: "Danial",
+    age: 25,
+    city: "Nalbari",
+    course: "Master of computer science",
+};
+console.log(student.age);
+let village;
+village = "barchenikuchi";
+console.log(village.toUpperCase());
+function users(dhiman01) {
+    if (dhiman01 < 10) {
+        console.log("he is a dumb ass");
+    }
+    else {
+        console.log("He has a marcedes");
+    }
+}
+users(100);
+//# sourceMappingURL=1_hello_world.js.map
---
+++ src/1_hello_world.ts
@@ -0,0 +1,32 @@
+let dhiman: string = "Dhiman";
+console.log(dhiman);
+
+// finding the typeof any variable
+const chris = 45;
+console.log(typeof chris);
+
+// creating object property
+const student = {
+  name: "Danial",
+  age: 25,
+  city: "Nalbari",
+  course: "Master of computer science",
+};
+
+console.log(student.age);
+
+// somne string operation
+let village: string;
+village = "barchenikuchi";
+console.log(village.toUpperCase());
+
+// if else statement
+function users(dhiman01: number) {
+  if (dhiman01 < 10) {
+    console.log("he is a dumb ass");
+  } else {
+    console.log("He has a marcedes");
+  }
+}
+// calling the function
+users(100);
